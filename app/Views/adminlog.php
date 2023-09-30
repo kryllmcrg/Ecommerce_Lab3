269d461afd84d5fa9c325feecd99fc4aa0c3047d
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-10-01 01:01:12 +0800

    main

---
+++ app/Views/adminlog.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?= view('admin/adminlogin/header'); ?>
+<body>
+<?= view('admin/adminlogin/loginbody'); ?>
+</body>
+</html> 
---
+++ app/Views/ecommerce.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?= view('ecommerce/header'); ?>
+<body>
+<?= view('ecommerce/body'); ?>
+<?= view('ecommerce/footer'); ?>
+</body>
+</html>
