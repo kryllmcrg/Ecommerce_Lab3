91498927062a71e34fbdddb58cd64edd73816ff2
kryllmcrg_18 <macaraigkarylle48@gmail.com>
2023-10-01 08:19:15 +0800

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,13 +7,13 @@ use CodeIgniter\Model;
 class UserModel extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'users';
+    protected $table            = 'products';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['id', 'name'];
 
     // Dates
     protected $useTimestamps = false;
